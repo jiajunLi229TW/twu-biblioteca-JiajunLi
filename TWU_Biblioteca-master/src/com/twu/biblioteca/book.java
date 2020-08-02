1350972b771f53964f5a2d2550dfc57bbe6fa101
jiajunLi229TW <jiajun.li1@thoughtworks.com>
2020-08-02 20:39:56 +0800

    [JiajunLi] feat: established book class.

---
+++ TWU_Biblioteca-master/src/com/twu/biblioteca/book.java
@@ -0,0 +1,14 @@
+package com.twu.biblioteca;
+
+public class book {
+    private  String name;
+    private  String author;
+    private  int publishedYear;
+
+    public void bookRegister (String name, String author, int publishedYear) {
+        this.name = name;
+        this.author = author;
+        this. publishedYear = publishedYear;
+    }
+
+}
